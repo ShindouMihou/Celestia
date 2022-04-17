106bd867470aef7601ce9af0f42226a6dfc77b26
Shindou Mihou <kokocrunchsh@gmail.com>
2022-04-17 16:35:41 +0800

    Rename hero1 to hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <div class="py-32 flex flex-col w-full min-h-screen bg-white rounded drop-shadow-md">
-    <div id="_hero1" class="m-auto">
-        <img src="/hero1.gif" class="m-auto" alt="Floating Books by Storyset"/>
+    <div id="hero" class="m-auto">
+        <img src="/hero.gif" class="m-auto" alt="Floating Books by Storyset"/>
         <div class="flex flex-col gap-6">
             <h2 class="text-5xl font-bold">Simplify the viewing <span class="text-slate-600">experience.</span></h2>
             <p class="text-xl font-light max-w-[720px]">A simple logging client that makes viewing through categorized logs neater, without the need for a complicated tech stack.</p>
