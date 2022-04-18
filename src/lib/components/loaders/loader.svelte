61a3328a6365d093127c0ac604d89a72aa6e8866
Shindou Mihou <kokocrunchsh@gmail.com>
2022-04-19 05:06:43 +0800

    Remove funky transition fade

--- src/lib/components/loaders/loader.svelte
+++ src/lib/components/loaders/loader.svelte
@@ -1,6 +1,1 @@
-<script>
-    import { fade } from "svelte/transition";
-</script>
-
-<div class="bg-black animate-pulse duration-500 rounded p-12 flex flex-col gap-4" transition:fade>
-</div>
+<div class="bg-black animate-pulse duration-500 rounded p-12 flex flex-col gap-4"></div>
