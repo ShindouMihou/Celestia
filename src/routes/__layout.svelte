c6cb4afba96dcd6dc8480d7f78edcb097da5c736
Shindou Mihou <kokocrunchsh@gmail.com>
2022-04-30 14:54:20 +0800

    Make the homepage more simpler in appearance.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,10 +5,5 @@
 </script>
 
 <div class="w-full p-4">
-  <Header>
-    <a href="https://github.com/ShindouMihou/Celestia">
-      <Github class="h-6 w-6 hover:text-blue-500 duration-300"></Github>
-    </a>
-  </Header>
   <slot />
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,26 @@
-<div class="py-32 flex flex-col w-full min-h-screen bg-white rounded drop-shadow-md">
+<script>
+import Header from "$lib/components/header.svelte";
+import Github from "$lib/components/icons/github.svelte";
+
+import { Icon, Globe } from "svelte-hero-icons";
+
+</script>
+<div class="py-32 px-4 flex flex-col w-full min-h-screen rounded">
     <div id="hero" class="m-auto">
-        <img src="/hero.gif" class="m-auto" alt="Floating Books by Storyset"/>
         <div class="flex flex-col gap-6">
-            <h2 class="text-5xl font-bold">Simplify the viewing <span class="text-slate-600">experience.</span></h2>
-            <p class="text-xl font-light max-w-[720px]">A simple logging client that makes viewing through categorized logs neater, without the need for a complicated tech stack.</p>
+            <h2 class="text-5xl font-bold">Hello.</h2>
+            <div class="flex flex-col gap-4">
+                <p class="text-xl font-light max-w-4xl">
+                    If you are part of the people authorized to use this instance, contact your administrator 
+                    for guidelines to access the dashboard otherwise consult the documentations from our GitHub.
+                </p>
+                <p class="font-light">Celestia { new Date().getFullYear() }</p>
+            </div>
+            <div class="flex flex-row gap-4 items-center">
+                <a href="https://github.com/ShindouMihou/Celestia">
+                    <Github class="h-6 w-6 hover:text-blue-500 duration-300"></Github>
+                </a>
+            </div>
         </div>
     </div>
 </div>
