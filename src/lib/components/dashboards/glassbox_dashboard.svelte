ff0147fa298ef182e35f1d9b39652711d6076b65
Shindou Mihou <kokocrunchsh@gmail.com>
2022-04-30 15:50:25 +0800

    Fix broken infinite scroll during initial dashboard

--- src/lib/components/dashboards/glassbox_dashboard.svelte
+++ src/lib/components/dashboards/glassbox_dashboard.svelte
@@ -2,179 +2,224 @@
     import Document from "$lib/components/document.svelte";
     import Loader from "$lib/components/loaders/loader.svelte";
     import { onMount } from "svelte";
-    import axios, { type AxiosResponse } from 'axios';
+    import axios, { type AxiosResponse } from "axios";
     import { fade } from "svelte/transition";
-    import decoder from '$lib/decoder'
+    import decoder from "$lib/decoder";
     import { catchAxiosError, reportTelemetry } from "$lib/telemetry";
-    import { UrlManipulator } from '$lib/url'
+    import { UrlManipulator } from "$lib/url";
     import { Icon, LightningBolt, QuestionMarkCircle } from "svelte-hero-icons";
 
     let glassboxes: any[];
     let glassbox: string;
     let next: string;
-    let loading: boolean = false
+    let loading: boolean = false;
     let scroller;
     let documents: any[];
     let url: UrlManipulator;
-    let page = 0
+    let page = 0;
 
-    let destinationPage = 0
+    let destinationPage = 0;
 
     onMount(async () => {
-        url =  new UrlManipulator(window);
+        url = new UrlManipulator(window);
 
-        axios.get('/api/glassboxes')
-            .then(response => glassboxes = response.data)
-            .catch(err => catchAxiosError(window, err));
+        axios
+            .get("/api/glassboxes")
+            .then((response) => (glassboxes = response.data))
+            .catch((err) => catchAxiosError(window, err));
 
-        url.ifPresent(2, (value: string) => glassbox = value);
+        url.ifPresent(2, (value: string) => (glassbox = value));
 
         if (!glassbox) {
             documents = [];
             return;
         }
 
-        load();
-
         scroller = new IntersectionObserver(async (entries) => {
-            if (entries[0].intersectionRatio <= 0) return
+            if (entries[0].intersectionRatio <= 0) return;
 
             if (next) {
-                if (loading) return
+                if (loading) return;
 
-                loadMore()
+                loadMore();
             }
-        })
+        });
 
-        scroller.observe(document.querySelector('#__tunnelVision'))
-    })
+        load(() => scroller.observe(document.querySelector("#__tunnelVision")));
+    });
 
     function loadMoreUntilDestinationPage() {
         if (page < destinationPage) {
-            loadMore(loadMoreUntilDestinationPage)
+            loadMore(loadMoreUntilDestinationPage);
         }
     }
 
     /**
-     * Performs a GET request to the specified URI and performs the given function if the 
+     * Performs a GET request to the specified URI and performs the given function if the
      * request is fulfilled otherwise catches the error and passes into telemetry.
-     * 
+     *
      * @param uri   The URI resource to request.
      * @param onFulfilled   The function to perform on successful.
      */
-    async function request(uri: string, onFulfilled: (value: AxiosResponse<any, any>) => any) {
-        return axios.get(uri)
-            .then(value => onFulfilled(value))
-            .catch(err => catchAxiosError(window, err));
+    async function request(
+        uri: string,
+        onFulfilled: (value: AxiosResponse<any, any>) => any
+    ) {
+        return axios
+            .get(uri)
+            .then((value) => onFulfilled(value))
+            .catch((err) => catchAxiosError(window, err));
     }
 
     /**
      * Transitions into the given glassbox.
-     * 
+     *
      * @param glassBoxName  The name of the glassbox to move into.
      */
     async function move(glassBoxName) {
-        loading = true
         glassbox = glassBoxName;
         page = 0;
         destinationPage = 0;
 
-
         documents = null;
-        url.searchParams.delete('query')
-        document.querySelector('#_filter').value = ''
-        window.history.pushState({}, '', `/dashboard/${glassbox}`);
-        url.searchParams.set('page', '0')
+        url.searchParams.delete("query");
+        document.querySelector("#_filter").value = "";
+        window.history.pushState({}, "", `/dashboard/${glassbox}`);
+        url.searchParams.set("page", "0");
 
         load();
     }
 
     /**
-     * Loads the data from the glassbox and allow Svelte to display the results 
+     * Loads the data from the glassbox and allow Svelte to display the results
      * into the page.
      */
-    async function load() {        
-        if (url.searchParams.get('query')) {
-            document.querySelector('#_filter').value = atob(url.searchParams.get('query'))
-            filter()
-            return
+    async function load(onFulfilled = () => {}) {
+        if (url.searchParams.get("query")) {
+            document.querySelector("#_filter").value = atob(
+                url.searchParams.get("query")
+            );
+            filter();
+            return;
         }
 
-        loading = true
-        return request(`/api/glassbox/${glassbox}`, (response) => {
+        loading = true;
+        request(`/api/glassbox/${glassbox}`, (response) => {
             documents = response.data.data;
             next = response.data.next;
 
-            if (url.searchParams.get('page')) {
-                destinationPage = Number.parseInt(url.searchParams.get('page'))
+            if (url.searchParams.get("page") != null) {
+                destinationPage = Number.parseInt(url.searchParams.get("page"));
 
-                loadMore(loadMoreUntilDestinationPage)
+                loadMore(loadMoreUntilDestinationPage);
             }
-            loading = false
-        }).then(() => document.querySelector('#documents').scrollIntoView({
-            behavior: 'smooth'
-        }));
+
+        }).then(() => {
+            loading = false;
+
+            onFulfilled()
+            document.querySelector("#documents").scrollIntoView({
+                behavior: "smooth",
+            });
+        });
     }
 
     /**
-     * Loads more results of the current result, this requires the next variable 
+     * Loads more results of the current result, this requires the next variable
      * to be instiated and of a non-faulty value.
      */
     async function loadMore(onFulfilled = () => {}) {
         if (next) {
-            loading = true
-            scroller.unobserve(document.querySelector('#__tunnelVision'))
+            loading = true;
+            if (scroller == null) {
+                scroller = new IntersectionObserver(async (entries) => {
+                    if (entries[0].intersectionRatio <= 0) return;
+
+                    if (next) {
+                        if (loading) return;
+
+                        loadMore();
+                    }
+                });
+            }
+
+            scroller.unobserve(document.querySelector("#__tunnelVision"));
 
             request(next, (response) => {
                 documents = documents.concat(response.data.data);
                 next = response.data.next;
 
-                page += 1
-                url.searchParams.set('page', page.toString())
-                window.history.pushState({}, '', `/dashboard/${glassbox}${url.stringify(url.searchParams)}`);
+                page += 1;
+                url.searchParams.set("page", page.toString());
+                window.history.pushState(
+                    {},
+                    "",
+                    `/dashboard/${glassbox}${url.stringify(url.searchParams)}`
+                );
 
-                loading = false
-                scroller.observe(document.querySelector('#__tunnelVision'))
-                onFulfilled()
+                loading = false;
+                scroller.observe(document.querySelector("#__tunnelVision"));
+                onFulfilled();
             });
         }
     }
 
     /**
-     * Loads the results of the glassbox with a specific query given to filter out the results. 
+     * Loads the results of the glassbox with a specific query given to filter out the results.
      * This method accepts JSON and the simple Celebi-decoded which is a little modified variant of Dotenv files.
      * - You can use Number(value) as a value to a property to translate the value into a number.
      * - You can use Date(value) as a value to a property to translate the value into a date.
      * - Those decoders are case-sensitive.
      */
     async function filter() {
-        const query = document.querySelector('#_filter');
-        loading = true
+        const query = document.querySelector("#_filter");
+        loading = true;
         if (!query.value) {
-            url.searchParams.delete('query')
-            window.history.pushState({}, '', `/dashboard/${glassbox}${url.stringify(url.searchParams)}`);
+            url.searchParams.delete("query");
+            window.history.pushState(
+                {},
+                "",
+                `/dashboard/${glassbox}${url.stringify(url.searchParams)}`
+            );
 
             load();
             return;
         }
-        
-        if (glassbox) {            
-            request(`/api/glassbox/${glassbox}?json=${btoa(JSON.stringify(decoder(query)))}`, (response) => {
-                documents = response.data.data;
-                next = response.data.next;
 
-                url.searchParams.set('query', btoa(JSON.stringify(decoder(query))))
-                window.history.pushState({}, '', `/dashboard/${glassbox}${url.stringify(url.searchParams)}`);
-                loading = false
-            }).then(() => document.querySelector('#documents').scrollIntoView({
-                behavior: 'smooth'
-            }));
-            return
+        if (glassbox) {
+            request(
+                `/api/glassbox/${glassbox}?json=${btoa(
+                    JSON.stringify(decoder(query))
+                )}`,
+                (response) => {
+                    documents = response.data.data;
+                    next = response.data.next;
+
+                    url.searchParams.set(
+                        "query",
+                        btoa(JSON.stringify(decoder(query)))
+                    );
+                    window.history.pushState(
+                        {},
+                        "",
+                        `/dashboard/${glassbox}${url.stringify(
+                            url.searchParams
+                        )}`
+                    );
+                    loading = false;
+                }
+            ).then(() =>
+                document.querySelector("#documents").scrollIntoView({
+                    behavior: "smooth",
+                })
+            );
+            return;
         }
 
-        loading = false
+        loading = false;
     }
 </script>
+
 <svelte:head>
     <title>Celestia | Dashboard</title>
 </svelte:head>
@@ -184,13 +229,26 @@
         <div class="flex flex-col p-3 gap-2">
             <div class="flex flex-row gap-1 items-center">
                 <h2 class="text-lg font-bold">Filter</h2>
-                <a href="https://github.com/ShindouMihou/Celestia/blob/master/documentations/filters.md" rel="external" target="_blank">
-                    <Icon src={QuestionMarkCircle} class="h-4 w-4 hover:text-blue-500 duration-300"></Icon>
+                <a
+                    href="https://github.com/ShindouMihou/Celestia/blob/master/documentations/filters.md"
+                    rel="external"
+                    target="_blank"
+                >
+                    <Icon
+                        src={QuestionMarkCircle}
+                        class="h-4 w-4 hover:text-blue-500 duration-300"
+                    />
                 </a>
             </div>
-            <textarea class="p-4 text-md font-light bg-gray-100 rounded outline-none" placeholder="FIELD=VALUE" id="_filter"></textarea>
+            <textarea
+                class="p-4 text-md font-light bg-gray-100 rounded outline-none"
+                placeholder="FIELD=VALUE"
+                id="_filter"
+            />
             <button class="cursor-pointer" on:click={filter} transition:fade>
-                <div class="bg-slate-100 border text-black rounded p-2 flex flex-col hover:opacity-80 gap-4 lg:w-32">
+                <div
+                    class="bg-slate-100 border text-black rounded p-2 flex flex-col hover:opacity-80 gap-4 lg:w-32"
+                >
                     <h3 class="font-medium ">Filter</h3>
                 </div>
             </button>
@@ -203,16 +261,25 @@
                 <div class="flex flex-col gap-1" id="glassboxes">
                     {#if glassboxes == null}
                         {#each { length: 5 } as _, i}
-                        <Loader></Loader>
+                            <Loader />
                         {/each}
                     {:else}
                         {#each glassboxes as item}
-                        <div class="cursor-pointer h-full" on:click={move(item.name)} transition:fade>
-                            <div class="component-box">
-                                <h3 class="font-bold uppercase">{item.name}</h3>
-                                <p>{Intl.NumberFormat().format(item.count)} documents</p>
+                            <div
+                                class="cursor-pointer h-full"
+                                on:click={move(item.name)}
+                                transition:fade
+                            >
+                                <div class="component-box">
+                                    <h3 class="font-bold uppercase">
+                                        {item.name}
+                                    </h3>
+                                    <p>
+                                        {Intl.NumberFormat().format(item.count)}
+                                        documents
+                                    </p>
+                                </div>
                             </div>
-                        </div>
                         {/each}
                     {/if}
                 </div>
@@ -221,23 +288,33 @@
         <div class="border rounded md:w-96 flex-grow">
             <div class="flex flex-col gap-4 p-4">
                 <h2 class="text-lg font-bold">Documents</h2>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 2xl:grid-cols-2 gap-2" id="documents">
+                <div
+                    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 2xl:grid-cols-2 gap-2"
+                    id="documents"
+                >
                     {#if documents == null}
                         {#each { length: 5 } as _, i}
-                        <Loader></Loader>
+                            <Loader />
                         {/each}
                     {:else}
                         {#each documents as item}
-                        <Document glassbox="{glassbox}" event="{item._event}" id="{item._id}" date="{item._date}"></Document>
+                            <Document
+                                {glassbox}
+                                event={item._event}
+                                id={item._id}
+                                date={item._date}
+                            />
                         {/each}
                     {/if}
                 </div>
-                <div id="__tunnelVision"/>
+                <div id="__tunnelVision" />
                 {#if loading}
-                <div class="w-full align-middle justify-center flex flex-row gap-4 m-auto py-6 animate-bounce">
-                    <Icon src={LightningBolt} class="h-6 w-6" solid/>
-                    <p>Loading...</p>
-                </div>
+                    <div
+                        class="w-full align-middle justify-center flex flex-row gap-4 m-auto py-6 animate-bounce"
+                    >
+                        <Icon src={LightningBolt} class="h-6 w-6" solid />
+                        <p>Loading...</p>
+                    </div>
                 {/if}
             </div>
         </div>
